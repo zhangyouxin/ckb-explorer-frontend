cd4d0a9df8c70a2bd43054ea17ab6459e8dac7bb
zhangyouxin <zhangyouxingg@126.com>
2023-12-07 10:43:36 +0800

    chore: add comment for display count

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -113,6 +113,7 @@ const Search: FC<{
   truncateTypeHash?: boolean
   onEditEnd?: () => void
 }> = memo(({ content, hasButton, onEditEnd, truncateTypeHash }) => {
+  // display only ten records for search by name
   const DISPLAY_COUNT = 10
   const isMobile = useIsMobile()
   const { t } = useTranslation()

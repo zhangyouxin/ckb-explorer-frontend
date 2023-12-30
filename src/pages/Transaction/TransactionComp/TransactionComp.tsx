a348ffadcce01a0aa917f21336cb7a3dc7ba7222
zhangyouxin <zhangyouxingg@126.com>
2023-12-30 20:39:40 +0800

    chore: use pagination in url

--- src/pages/Transaction/TransactionComp/TransactionComp.tsx
+++ src/pages/Transaction/TransactionComp/TransactionComp.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 import TransactionCellList from '../TransactionCellList'
 import { Cell } from '../../../models/Cell'
 import { Transaction } from '../../../models/Transaction'
@@ -24,16 +25,40 @@ const handleCellbaseInputs = (inputs: Cell[], outputs: Cell[]) => {
 
 export const TransactionComp = ({ transaction }: { transaction: Transaction }) => {
   const txHash = transaction.transactionHash
-  // refactor to configurable
-  const PAGE_SIZE = 10
-  const [inputCellsPage, setInputCellsPage] = useState(1)
+  const location = useLocation()
+  const history = useHistory()
+  const queryParams = new URLSearchParams(location.search)
+  const inputCellsPageNumber = queryParams.get('inputCellsPageNumber')
+  const inputCellsPageSize = queryParams.get('inputCellsPageSize')
+  const outputCellsPageNumber = queryParams.get('outputCellsPageNumber')
+  const outputCellsPageSize = queryParams.get('outputCellsPageSize')
+
+  const DEFAULT_PAGE_SIZE = 10
+  const inputCellsPageLimit = inputCellsPageSize ? Number(inputCellsPageSize) : DEFAULT_PAGE_SIZE
+  const outputCellsPageLimit = outputCellsPageSize ? Number(outputCellsPageSize) : DEFAULT_PAGE_SIZE
+  const [inputCellsPage, setInputCellsPage] = useState(inputCellsPageNumber ? Number(inputCellsPageNumber) : 1)
+  const handleInputCellsPageChange = (page: number) => {
+    setInputCellsPage(page)
+    queryParams.set('inputCellsPageNumber', page.toString())
+    history.replace(`${location.pathname}?${queryParams.toString()}`)
+  }
   const txInputsQuery = useQuery(['transactionInputs', txHash, inputCellsPage], async () => {
-    const result = await explorerService.api.fetchTransactionInputsByHash(txHash, inputCellsPage, PAGE_SIZE)
+    const result = await explorerService.api.fetchTransactionInputsByHash(txHash, inputCellsPage, inputCellsPageLimit)
     return result
   })
-  const [outputCellsPage, setOutputCellsPage] = useState(1)
+  const [outputCellsPage, setOutputCellsPage] = useState(outputCellsPageNumber ? Number(outputCellsPageNumber) : 1)
+  const handleOutputCellsPageChange = (page: number) => {
+    setOutputCellsPage(page)
+    queryParams.set('outputCellsPageNumber', page.toString())
+    history.replace(`${location.pathname}?${queryParams.toString()}`)
+  }
+
   const txOutputsQuery = useQuery(['transactionOutputs', txHash, outputCellsPage], async () => {
-    const result = await explorerService.api.fetchTransactionOutputsByHash(txHash, outputCellsPage, PAGE_SIZE)
+    const result = await explorerService.api.fetchTransactionOutputsByHash(
+      txHash,
+      outputCellsPage,
+      outputCellsPageLimit,
+    )
     // TODO: When will displayOutputs be empty? Its type description indicates that it will not be empty.
     if (result.data.length > 0) {
       result.data[0].isGenesisOutput = transaction.blockNumber === 0
@@ -54,26 +79,22 @@ export const TransactionComp = ({ transaction }: { transaction: Transaction }) =
       <div className="transactionInputs">
         {txInputsQuery.isFetching && <Loading show />}
         {inputs && <TransactionCellList inputs={inputs} showReward={Number(blockNumber) > 0 && isCellbase} />}
-        {txInputsQuery.isSuccess && txInputsQuery.data.total > PAGE_SIZE && (
-          <Pagination
-            currentPage={inputCellsPage}
-            totalPages={txInputsQuery.isSuccess ? Math.ceil(txInputsQuery.data.total / PAGE_SIZE) : 1}
-            onChange={setInputCellsPage}
-          />
-        )}
+        <Pagination
+          currentPage={inputCellsPage}
+          totalPages={txInputsQuery.isSuccess ? Math.ceil(txInputsQuery.data.total / inputCellsPageLimit) : 1}
+          onChange={handleInputCellsPageChange}
+        />
       </div>
       <div className="transactionOutputs">
         {txOutputsQuery.isFetching && <Loading show />}
         {txOutputsQuery.isSuccess && (
           <TransactionCellList outputs={txOutputsQuery.data.data} txHash={transactionHash} />
         )}
-        {txOutputsQuery.isSuccess && txOutputsQuery.data.total > PAGE_SIZE && (
-          <Pagination
-            currentPage={outputCellsPage}
-            totalPages={txOutputsQuery.isSuccess ? Math.ceil(txOutputsQuery.data.total / PAGE_SIZE) : 1}
-            onChange={setOutputCellsPage}
-          />
-        )}
+        <Pagination
+          currentPage={outputCellsPage}
+          totalPages={txOutputsQuery.isSuccess ? Math.ceil(txOutputsQuery.data.total / outputCellsPageLimit) : 1}
+          onChange={handleOutputCellsPageChange}
+        />
       </div>
     </>
   )
